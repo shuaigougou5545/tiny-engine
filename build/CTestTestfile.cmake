aef06151b812f1b72d34f46f2d8ac755677f1bc8
shuaigougou5545 <737224178@qq.com>
2023-12-23 18:13:07 +0800

    fix：add gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/goujiayuan/STUDY/图形学算法‼️/tiny-engine
-# Build directory: /Users/goujiayuan/STUDY/图形学算法‼️/tiny-engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
